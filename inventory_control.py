0ffe1d448a12cdb574c4c161abdec1e3b6471e17
Bryan 'Max' Dobberstein <max@launchcode.org>
2017-09-20 13:52:05 -0500

    Create item table class, setup many-to-one rel with category

--- inventory_control.py
+++ inventory_control.py
@@ -13,9 +13,28 @@ db = SQLAlchemy(app)
 class Category(db.model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(30))
+    item = db.Relationship("Item", backref = "category". lazy = "dynamic")
 
     def __init__(self, name):
         self.name = name
+
+class Item(db.model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(30))
+    description = db.Column(db.Text)
+    serial_number = db.Column(db.string(50))
+    sku = db.Column(db.String(50))
+    price = db.Column(db.Float)
+    location = db.Column(db.Text)
+    category_id = db.Column(db.Integer, db.Foreignkey("category.id"))
+
+    def __init__(self, name, description, sku, price, location, serial_number = "NA"):
+        self.name = name
+        self.description = description
+        self.sku = sku
+        self.price = price
+        self.location = location
+        self.serial_number = serial_number
 
 if __name__ == "__main__":
     app.run()
