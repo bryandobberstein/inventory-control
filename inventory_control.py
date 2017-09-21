82dfe8c40236d0941afa699503a6f6b891ae3ff5
Bryan 'Max' Dobberstein <max@launchcode.org>
2017-09-21 15:47:04 -0500

    Add placeholder form to placeholder index.html

--- inventory_control.py
+++ inventory_control.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////./database/inventory.db"
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = "JinUdceer0"
+app.config["SECRET_KEY"] = "JinUdceer0"
 
 db = SQLAlchemy(app)
 
@@ -36,9 +38,22 @@ class Item(db.Model):
         self.location = location
         self.serial_number = serial_number
 
-@app.route("/")
+class TestForm(FlaskForm):
+    name = StringField("Name")
+    submit = SubmitField("Enter")
+
+@app.route("/", methods = ["GET", "POST"])
 def index():
-    return render_template("index.html")
+    form = TestForm()
+    name = ""
+    greeting = "Enter Your Name"
+    if request.method == "POST":
+        if form.name.data:
+            name = form.name.data
+            greeting = "Hello, {}".format(name)
+        else:
+            greeting = "NOTHING ENTERED"
+    return render_template("index.html", form = form, greeting = greeting)
 
 if __name__ == "__main__":
     app.run()
