88f0c16a988afe9a5ab649c2778a08fa69af2c64
Bryan 'Max' Dobberstein <max@launchcode.org>
2017-09-21 22:05:52 -0500

    Add wtform macro, test werzeug.security

--- inventory_control.py
+++ inventory_control.py
@@ -2,10 +2,12 @@ from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired
+from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////./database/inventory.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home/max/Documents/git/inventory-control/database/inventory.db"
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "JinUdceer0"
@@ -39,7 +41,7 @@ class Item(db.Model):
         self.serial_number = serial_number
 
 class TestForm(FlaskForm):
-    name = StringField("Name")
+    name = StringField("Name", validators = [InputRequired(message = "Name required")])
     submit = SubmitField("Enter")
 
 @app.route("/", methods = ["GET", "POST"])
@@ -48,9 +50,11 @@ def index():
     name = ""
     greeting = "Enter Your Name"
     if request.method == "POST":
-        if form.name.data:
+        if form.validate_on_submit():
             name = form.name.data
-            greeting = "Hello, {}".format(name)
+            hashed_name = generate_password_hash(name, method="sha512", salt_length = 10)
+            check = check_password_hash(hashed_name, name)
+            greeting = "Hello, {}. {}. {}".format(name, hashed_name, check)
         else:
             greeting = "NOTHING ENTERED"
     return render_template("index.html", form = form, greeting = greeting)
