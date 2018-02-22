79fadf858ce1ba54b8f01920b46666441766edb5
max <you@example.com>
2018-02-22 09:34:13 -0600

    abstract password encryption functions to own module

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from flask import request, redirect, render_template, session
-from flask_bcrypt import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required, current_user
 from inventory_control import app, lm
+from password_tools import hashpw, checkpw
 from forms import *
 from models import *
 
@@ -20,9 +20,9 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username = form.username.data).first()
         password = form.password.data
-        if user is None or not check_password_hash(user.pwhash, password):
+        if user is None or not checkpw(user.pwhash, password):
             return redirect("login")
-        elif check_password_hash(user.pwhash, password):
+        elif checkpw(user.pwhash, password):
             login_user(user, form.remember_me.data)
             if int(user.new_user) == 1:
                 return redirect("change_pw")
@@ -37,14 +37,14 @@ def change_pw():
     username = current_user.username
     if form.validate_on_submit():
         password = form.password.data
-        pwhash = generate_password_hash(password)
+        pwhash = hashpw(password)
         user = User.query.filter_by(username = username).first()
         user.new_user = 0
         db.session.commit()
         return redirect("search")
     return render_template("change_pw.html", form = form, greeting = "Change Password", new_user = 1)
 
-@app.route("/lougout", methods = ["GET", "POST"])
+@app.route("/logout", methods = ["GET", "POST"])
 @login_required
 def logout():
     logout_user()
